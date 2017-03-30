148babc280c1dda5d57bd68cff47692f223353cb
azu <azuciao@gmail.com>
2017-03-30 19:55:15 +0900

    Add ChangeContext

--- src/component/App.tsx
+++ src/component/App.tsx
@@ -1,22 +1,22 @@
 // LICENSE : MIT
 "use strict";
 import React from "react";
+import { Context, Dispatcher } from "almin";
+const AlminLogger = require("almin-logger");
 import Counter from './Counter';
 import Loading from './Loading/Loading';
-import { Context, Payload, Dispatcher, DispatcherPayloadMeta } from "almin";
-const AlminLogger = require("almin-logger");
-import { AppUseCaseType, AppUseCase } from "../use-case/AppUseCase";
+import { Changdable } from "../use-case/AppUseCase";
 import { AppStoreGroup } from "../store/AppStoreGroup";
 import { AppStoreGroupState } from "../store/AppStoreGroup";
-import { DispatchedPayload } from "almin/lib/Dispatcher";
 const store = AppStoreGroup.create();
-const context = new Context({
+const ChangedableContext = Changdable(Context);
+const context = new ChangedableContext({
     dispatcher: new Dispatcher(),
     store
 });
+
 const logger = new AlminLogger();
 logger.startLogging(context);
-
 
 export default class App extends React.Component<{}, AppStoreGroupState> {
     constructor() {
@@ -25,17 +25,6 @@ export default class App extends React.Component<{}, AppStoreGroupState> {
     }
 
     componentDidMount() {
-        // UseCase#changed -> Update State
-        context.onDispatch((payload: Payload) => {
-            if (payload.type === AppUseCaseType) {
-                this.setState(context.getState());
-            }
-        });
-        context.onCompleteEachUseCase((_, meta) => {
-            if (meta.useCase instanceof AppUseCase) {
-                this.setState(context.getState());
-            }
-        });
         // when change store, update component
         const onChangeHandler = () => {
             this.setState(context.getState());
--- src/use-case/AppUseCase.ts
+++ src/use-case/AppUseCase.ts
@@ -1,10 +1,43 @@
-import { UseCase } from "almin";
+import { UseCase, Dispatcher, Payload, Context } from "almin";
 export const AppUseCaseType = "APP_CHANGED";
+
+// changed
+type Constructor<T> = new (...args: Array<any>) => T;
+// etends Context
+class AppChangePayload extends Payload {
+    constructor() {
+        super({ type: AppUseCaseType });
+    }
+}
+const appChangePayload = new AppChangePayload();
+export function Changdable<T extends Constructor<Context>>(Base: T) {
+    return class extends Base {
+        dispatcher: Dispatcher;
+        handler: () => void;
+        constructor(...args: Array<any>) {
+            super(...args);
+            const dispatcher: Dispatcher = args[0].dispatcher;
+            this.dispatcher = dispatcher;
+            // UseCase#changed -> Update State
+            this.dispatcher.onDispatch((payload: Payload) => {
+                if (payload.type === AppUseCaseType) {
+                    this.handler()
+                }
+            });
+            this.onCompleteEachUseCase(() => {
+                this.handler();
+            });
+        }
+
+        onChange(handler: () => void) {
+            this.handler = handler;
+        }
+    };
+}
+
 export class AppUseCase extends UseCase {
     changed() {
         // Just in time updating..
-        this.dispatch({
-            type: AppUseCaseType
-        });
+        this.dispatch(appChangePayload);
     }
 }
