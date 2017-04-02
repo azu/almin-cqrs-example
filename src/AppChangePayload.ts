db89f6bce77f15bdbae0829cde61251979e1abea
azu <azuciao@gmail.com>
2017-04-02 15:30:46 +0900

    refactor(StoreGroup): refactor StoreGroup

--- src/AppChangePayload.ts
+++ src/AppChangePayload.ts
@@ -1,6 +1,5 @@
 import { Payload } from 'almin';
 const AppUseCaseType = "APP_CHANGED";
-// etends Context
 export class AppChangePayload extends Payload {
     constructor() {
         super({ type: AppUseCaseType });
--- src/store/CQRSStoreGroup.ts
+++ src/store/CQRSStoreGroup.ts
@@ -1,20 +1,37 @@
 // MIT © 2017 azu
 "use strict";
 import * as assert from "assert";
-import { Store, Payload, DidExecutedPayload, CompletedPayload } from "almin";
+import { Store, Payload, DidExecutedPayload, CompletedPayload, DispatcherPayloadMeta, ErrorPayload } from "almin";
 import { AppChangePayload } from "../AppChangePayload";
+import MapLike from "map-like";
 const CHANGE_STORE_GROUP = "CHANGE_STORE_GROUP";
+/**
+ * onChange flow
+ * https://code2flow.com/N5yToZ
+ */
 export class CQRSStoreGroup extends Store {
     public stores: Array<Store>;
+    // current changing stores for emitChange
+    private _changingStores: Array<Store> = [];
+    // all functions to release handlers
     private _releaseHandlers: Array<Function> = [];
 
     constructor(stores: Array<Store>) {
         super();
         this.stores = stores;
-        this._startObservePayload();
+
+        // Implementation Note:
+        // Dispatch -> pipe -> Store#emitChange() if it is needed
+        //          -> this.onDispatch -> If anyone store is changed, this.emitChange()
+        // each pipe to dispatching
         stores.forEach(store => {
             this._registerStore(store);
+            // delegate dispatching
+            const pipeHandler = this.pipe(store);
+            this._releaseHandlers.push(pipeHandler);
         });
+        // after dispatching, and then emitChange
+        this._startObservePayload();
     }
 
     /**
@@ -53,7 +70,8 @@ StoreGroup#getState()["StateName"]; // state
      * @public
      */
     emitChange(): void {
-        this.emit(CHANGE_STORE_GROUP, this.stores.slice());
+        this.emit(CHANGE_STORE_GROUP, this._changingStores.slice());
+        this._pruneChangingStores();
     }
 
     /**
@@ -77,35 +95,61 @@ StoreGroup#getState()["StateName"]; // state
     release(): void {
         this._releaseHandlers.forEach(releaseHandler => releaseHandler());
         this._releaseHandlers.length = 0;
+        this._pruneChangingStores();
     }
 
     /**
      * register store and listen onChange.
-     * If you release store, and do call {@link release} method.
+     * If you release store, and do call `release` method.
      * @param {Store} store
      * @private
      */
-    private _registerStore(store: Store) {
-        // if anyone store is changed, will call `emitChange()`.
+    private _registerStore(store: Store): void {
+        // If anyone store is changed, will call `emitChange()`.
+        // It is means that
+        // 1. UseCase#dispatch -> Store#onDispatch
+        // 2. Store#emitChange -> Store#onChange
+        // 3. StoreGroup#emitChange -> Context#onChange
+        // 4. Context#getState -> get change state by dispatched
         const releaseOnChangeHandler = store.onChange(() => {
-            this.emit(CHANGE_STORE_GROUP, [store]);
+            this._addChangingStore(store);
         });
-        // Implementation Note:
-        // Delegate dispatch event to Store from StoreGroup
-        // Dispatcher -> StoreGroup -> Store
-        const releaseOnDispatchHandler = this.pipe(store);
         // add release handler
-        this._releaseHandlers = this._releaseHandlers.concat([releaseOnChangeHandler, releaseOnDispatchHandler]);
+        this._releaseHandlers.push(releaseOnChangeHandler);
     }
 
     // register changed events
-    _startObservePayload() {
-        this.onDispatch((payload: Payload | AppChangePayload) => {
-            if (payload instanceof DidExecutedPayload
-                || payload instanceof CompletedPayload
-                || payload instanceof AppChangePayload) {
+    _startObservePayload(): void {
+        const useCaseFinishedMap = new MapLike<string, boolean>();
+        const observeChangeHandler = (payload: Payload, meta: DispatcherPayloadMeta) => {
+            if (!meta.isTrusted) {
                 this.emitChange();
+            } else if (payload instanceof AppChangePayload) {
+                this.emitChange();
+            } else if (payload instanceof ErrorPayload) {
+                this.emitChange(); // MayBe
+            } else if (payload instanceof DidExecutedPayload && meta.useCase && meta.isUseCaseFinished) {
+                useCaseFinishedMap.set(meta.useCase.id, true);
+                this.emitChange(); // MayBe
+            } else if (payload instanceof CompletedPayload && meta.useCase && meta.isUseCaseFinished) {
+                // if the useCase is already finished, doesn't emitChange in CompletedPayload
+                if (useCaseFinishedMap.has(meta.useCase.id)) {
+                    useCaseFinishedMap.delete(meta.useCase.id);
+                    return;
+                }
+                this.emitChange(); // MayBe
             }
-        });
+        };
+        const releaseHandler = this.onDispatch(observeChangeHandler);
+        this._releaseHandlers.push(releaseHandler);
     }
+
+    private _addChangingStore(store: Store) {
+        this._changingStores.push(store);
+    }
+
+    private _pruneChangingStores() {
+        this._changingStores = [];
+    }
+
 }
